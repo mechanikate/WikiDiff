5dab41b0f36c3eaa3ecaab502949bbe025f70ba9
mechanikate <cjmatthy200@gmail.com>
2024-07-09 16:26:53 -0400

    refactor & support error cases

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from flask import Flask, request, render_template
-from difflib import Differ 
-import requests
-import collections
-import datetime
+import requests, collections, datetime
+from typing import List
 
-differ=Differ()
-app = Flask(__name__)
-cache = {}
-# https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=Pet_door&formatversion=2&rvprop=content&rvslots=*&rvstart=2023-06-02T01%3A44%3A00.000Z&rvend=2024-07-09T01%3A44%3A00.000Z&rvdir=newer
-def request_url(wikibaseurl="https://en.wikipedia.org/w/api.php", pagetitle="Donald_Trump", starttime="2020-02-09T02:42:57.512Z", endtime="2024-07-09T01:44:00.000Z"):
+app = Flask(__name__) # Get Flask ready
+cache = {} # Initialize the cache to nothing
+
+def request_url(wikibaseurl="https://en.wikipedia.org/w/api.php", pagetitle = "Python_(programming_language)", starttime="2020-02-09T02:42:57.512Z", endtime="2024-07-09T01:44:00.000Z"): # Get all the revisions (up to 10?) of a wikimedia page (by default Wikipedia)
     if(pagetitle in cache.keys()):
-        return cache[pagetitle]
-    resp = requests.get(wikibaseurl, params={
+        return cache[pagetitle] # If the page title is in the cache, just return what we had there
+    resp = requests.get(wikibaseurl, params={ # Otherwise, do the GET request!
         'action': 'query',
         'format': 'json',
         'prop': 'revisions',
@@ -24,44 +21,51 @@ def request_url(wikibaseurl="https://en.wikipedia.org/w/api.php", pagetitle="Don
         'rvdir': 'older',
         'rvlimit': 50
     })
-    rev = resp.json()["query"]["pages"][0]["revisions"]
+    print(resp.status_code)
+    if resp.status_code != 200:
+        return ["Oops! Something went wrong."]*10
+    rev = resp.json()["query"]["pages"][0]["revisions"] # Go through the JSON 'till we get here
     if not pagetitle in cache.keys():
-        cache[pagetitle] = rev
-    return rev
-def compare(dataarg, start=0, end=3, minimum_prevalence=1):
+        cache[pagetitle] = rev # If it's not in the cache, add it
+    return rev # Return the revisions
+def compare(dataarg: List[str], start=0, end=3, minimum_prevalence=1) -> List[str]:
     curr=dataarg[start].split("\n")
     for i in range(start+1,end):
-        curr.extend(dataarg[i].split("\n"))
-    dupes = [item for item, count in collections.Counter(curr).items() if count > int(minimum_prevalence)]
-    dupes=list(set(dupes))
+        curr.extend(dataarg[i].split("\n")) # Add every revision text to one list
+    dupes = [item for item, count in collections.Counter(curr).items() if count > int(minimum_prevalence)] # If text is seen more than minimum_prevalence times, then add it into dupes
+    dupes=list(set(dupes)) # Get unique names
     dupes.sort()
     return dupes
-def controversialness(dataarg, dupes):
+
+def controversialness(dataarg: str, dupes: List[str]) -> float:
     return max(0,int((1-len(dupes)/len(dataarg.split("\n")))*1000)/10)
-select_info = lambda txt: txt["slots"]["main"]["content"]
-def fullparse(pagetitle,minimum_prevalence=0.25, endtime=datetime.datetime.now().isoformat(), maxarticles=3):
+
+select_info = lambda txt: txt["slots"]["main"]["content"] # Used for getting even deeper into each revision's JSON (from request_url[i] to actual text)
+def fullparse(pagetitle,minimum_prevalence=0.25, maxarticles=3) -> List[str]: # Bring it all together!
     data = request_url(pagetitle=pagetitle)
     data = [select_info(i) for i in data]
     return compare(data,end=maxarticles,minimum_prevalence=minimum_prevalence)
 
 
-@app.route('/')
-def index():
-    return render_template("index.html")
+@app.route('/') # https://example.com
+def index_page():
+    return render_template("index.html") # templates/index.html
     
-@app.route('/w')
-def article_minprev_articleno():
+@app.route('/w') # https://example.com/w?article=...&minprev=...&articleno=...
+def get_common_article_text_page():
     return "\n".join(fullparse(request.args.get("article"), minimum_prevalence=float(request.args.get("minprev")), maxarticles=int(request.args.get("articleno"))))
 
-@app.route('/controversialness')
-def controversialness_page():
+@app.route('/controversialness') # https://example.com/controversialness?article=...&minprev=...&articleno=...
+def controversialness_page(): # Literally just returns a percent of what amount of the text has changed over 16:07
+    if(request.args.get("minprev") in ["", None] or request.args.get("articleno") in ["", None] or request.args.get("article") in ["", None]):
+        return "0%"
     data=fullparse(request.args.get("article"), minimum_prevalence=float(request.args.get("minprev")), maxarticles=int(request.args.get("articleno")))
     return str(controversialness(
         select_info(request_url(pagetitle=request.args.get("article"))[-1]),
         data
     ))+"%"
-@app.route('/render')
+@app.route('/render') # https://example.com/render?article=... (generally)
 def render():
-    return render_template("render.html")
+    return render_template("render.html") # templates/render.html
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+  app.run(host='0.0.0.0', port=5000) # Run it!
